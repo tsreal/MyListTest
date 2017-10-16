5f07722349ae06d95e0e9112088f3a6cf091abb0
tsreal <virus_tt@163.com>
2017-10-16 22:24:25 +0800

    添加三个界面设计代码

---
+++ app/src/main/java/tgtiger/cf/mylisttest/model/Item.java
@@ -0,0 +1,29 @@
+package tgtiger.cf.mylisttest.model;
+
+/**
+ * Created by Alex on 2017/10/16.
+ * 创建item模型类
+ */
+
+
+public class Item {
+
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
